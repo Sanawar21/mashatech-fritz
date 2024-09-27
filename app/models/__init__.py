b2bd8f1a4cdc0d70930ea11363bf4abc76d215ea
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-27 21:42:26 +0500

    add datahandler and messageid to __init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,5 @@ from .ad import Ad
 from .parser import AdParser, Match
 from .kleinanzeigen import KleinanzeigenClient
 from .telegram import TelegramClient
+from .data_handler import DataHandler
+from .message_id import MessageID
