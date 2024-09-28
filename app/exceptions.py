2007696be4060f7825df9f948d0ac18cfa641aa0
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-28 11:43:57 +0500

    create exceptions module

---
+++ app/exceptions.py
@@ -0,0 +1,11 @@
+
+class InvalidIncomingMessageException(Exception):
+    pass
+
+
+class InvalidAdException(Exception):
+    """Exception raised for unsuitable ads."""
+
+
+class InvalidOfferStatusException(Exception):
+    pass
