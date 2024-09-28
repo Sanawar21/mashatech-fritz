d14a7d3ff50b5809958ef281107a16ef52d4e51d
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-28 11:45:34 +0500

    add implementation of process method for OfferSentMessage

--- app/websocket/messages/incoming/offer_sent.py
+++ app/websocket/messages/incoming/offer_sent.py
@@ -1,17 +1,21 @@
 from ..base import IncomingMessage
+from ....models import MessageIDCache
 
 
 class OfferSentMessage(IncomingMessage):
     type_ = "offerSentAlert"
+    __cache = MessageIDCache()
 
     def __init__(self, message_id: str) -> None:
+        """When the extension sends and offer to the ad poster, 
+        the extension will send this message to the websocket."""
+
         self.message_id = message_id
-        super().__init()
+        super().__init__()
 
     @classmethod
     def from_dict(cls, data: dict):
         return cls(data.get('msg_id'))
 
     def process(self):
-        # TODO: Implement
-        pass
+        self.__cache.create_message_id(self.message_id)
