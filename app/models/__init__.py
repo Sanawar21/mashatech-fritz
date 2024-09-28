130f412c9e1d5aa299789b1d8022f2fa17257c21
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-28 11:13:54 +0500

    rename DataHandler class to MessageIDCache

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@ from .ad import Ad
 from .parser import AdParser, Match
 from .kleinanzeigen import KleinanzeigenClient
 from .telegram import TelegramClient
-from .data_handler import DataHandler
+from .cache import MessageIDCache
 from .message_id import MessageID
