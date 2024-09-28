57d820846f17334ece89f28c7de3915d5880a575
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-28 11:34:38 +0500

    add status attribute in MessageID object

--- app/models/message_id.py
+++ app/models/message_id.py
@@ -1,19 +1,22 @@
 class MessageID:
     """Message ID model that will be saved to the database."""
 
-    def __init__(self, message_id: str, timestamp: int) -> None:
+    def __init__(self, message_id: str, timestamp: int, status: str) -> None:
         self.message_id = message_id
         self.timestamp = timestamp
+        self.status = status
 
     def to_dict(self):
         return {
             "message_id": self.message_id,
-            "timestamp": self.timestamp
+            "timestamp": self.timestamp,
+            "status": self.status
         }
 
     @classmethod
     def from_dict(cls, data: dict):
         return cls(
             data.get('message_id'),
-            data.get('timestamp')
+            data.get('timestamp'),
+            data.get('status')
         )
