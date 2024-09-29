1e3abb4428f0a880b177f5106fdbdc7e4dc4416e
Sanawar_21 <sanawarsaeed@gmail.com>
2024-09-29 09:21:19 +0500

    add ad_id parameter to message_id object

--- app/models/message_id.py
+++ app/models/message_id.py
@@ -1,16 +1,18 @@
 class MessageID:
     """Message ID model that will be saved to the database."""
 
-    def __init__(self, message_id: str, timestamp: int, status: str) -> None:
+    def __init__(self, message_id: str, timestamp: int, status: str, ad_id: int) -> None:
         self.message_id = message_id
         self.timestamp = timestamp
         self.status = status
+        self.ad_id = ad_id
 
     def to_dict(self):
         return {
             "message_id": self.message_id,
             "timestamp": self.timestamp,
-            "status": self.status
+            "status": self.status,
+            "ad_id": self.ad_id,
         }
 
     @classmethod
@@ -18,5 +20,6 @@ class MessageID:
         return cls(
             data.get('message_id'),
             data.get('timestamp'),
-            data.get('status')
+            data.get('status'),
+            data.get('ad_id'),
         )
