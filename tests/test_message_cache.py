c405f8fb83f2cda2263a75174b334df4ba6cf432
Sanawar_21 <sanawarsaeed@gmail.com>
2024-10-02 22:47:30 +0500

    test messageIdCache

--- tests/test_message_cache.py
+++ tests/test_message_cache.py
@@ -8,6 +8,16 @@ msg_cache = MessageIDCache()
 # msg_cache.create("2", "2")
 # msg_cache.create("3", "3")
 
-# reading test
-print(msg_cache.read("1").to_dict())
-print([msg.to_dict() for msg in msg_cache.read_n_day_old(2)])
+# reading test: Success
+# print(msg_cache.read("1").to_dict())
+# print([msg.to_dict() for msg in msg_cache.read_n_day_old(2)])
+
+# updating test: Success
+# msg_cache.update_status("1", "accepted")
+# print([msg for msg in msg_cache.data])
+
+# deleting test: Success
+# msg_cache.delete("1")
+# msg_cache.delete("2")
+# msg_cache.delete("3")
+# print([msg for msg in msg_cache.data])
