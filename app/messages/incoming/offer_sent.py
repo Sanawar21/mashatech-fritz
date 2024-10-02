9e94a5e1e4bb333219d6d010435615f19c836026
Sanawar_21 <sanawarsaeed@gmail.com>
2024-10-02 20:35:22 +0500

    replace ad_uid with offer_id in from dict method

--- app/messages/incoming/offer_sent.py
+++ app/messages/incoming/offer_sent.py
@@ -9,14 +9,15 @@ class OfferSentMessage(IncomingMessage):
     __cache = MessageIDCache()
 
     def __init__(self, message_id: str, ad_uid: str) -> None:
-
+        # ad uid is offer_id
+        # message_id is conversation id of the chat
         self.message_id = message_id
         self.ad_uid = ad_uid
         super().__init__()
 
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(data.get('msg_id'), data.get('ad_uid'))
+        return cls(data.get('msg_id'), data.get('offer_id'))
 
     def process(self):
         self.__cache.create(self.message_id, self.ad_uid)
