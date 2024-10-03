8df5655978f83a472bf4ef291696c1c36af09930
Sanawar_21 <sanawarsaeed@gmail.com>
2024-10-03 20:44:26 +0500

    add implementation for deleting paid message ids

--- app/messages/outgoing/release_payment.py
+++ app/messages/outgoing/release_payment.py
@@ -1,6 +1,13 @@
 from ..base import ChatLinkMessage
+from ...cache import MessageIDCache
 
 
 class ReleasePaymentMessage(ChatLinkMessage):
     """Release the payment for an offer."""
     type_ = "releasePayment"
+    __cache = MessageIDCache()
+
+    def __init__(self, message_id) -> None:
+        super().__init__(message_id)
+        self.__cache.refresh()
+        self.__cache.delete(message_id)
