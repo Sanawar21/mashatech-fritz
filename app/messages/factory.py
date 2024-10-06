80b1218a8f862217f4ffa14b7d9c4f5665066a6e
Sanawar_21 <sanawarsaeed@gmail.com>
2024-10-06 12:48:48 +0500

    add message logging in factory

--- app/messages/factory.py
+++ app/messages/factory.py
@@ -3,12 +3,13 @@ from .base import IncomingMessage
 from ..exceptions import InvalidIncomingMessageException
 
 import json
-from json.decoder import JSONDecodeError
+import logging
 
 
 class MessageFactory:
     @staticmethod
     def create_message(message_data: str) -> IncomingMessage:
+        logging.info(f"Incoming message: {message_data}")
         try:
             message_data = json.loads(message_data)
             type_ = message_data.get('type')
@@ -17,6 +18,6 @@ class MessageFactory:
             for message in messages:
                 if message.type_ == type_:
                     return message.from_dict(message_data)
-        except (AttributeError, JSONDecodeError):
+        except (AttributeError, json.decoder.JSONDecodeError):
             raise InvalidIncomingMessageException("Message type not found")
         raise InvalidIncomingMessageException(f"Invalid message type: {type_}")
