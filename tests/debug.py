2a6c3e96e6c1cc6bb66a6e769b8490b74b28e706
Sanawar_21 <sanawarsaeed@gmail.com>
2024-11-19 17:32:05 +0500

    find the reason for undetected ad

---
+++ tests/debug.py
@@ -0,0 +1,13 @@
+from app.clients import KleinanzeigenClient
+from app.models.parser import AdParser
+
+kc = KleinanzeigenClient()
+ad = kc.get_ad("2923849354")
+p = AdParser()
+print(p.get_offer_price())
+print("Cause:")
+print(f"{ad.account_age <= 7=}")
+print(f"{ad.is_damaged=}")
+print(f"{ad.is_only_pickup=}")
+print(f"{not ad.price=}")
+print(f"{not ad.is_buy_now_available=}")
