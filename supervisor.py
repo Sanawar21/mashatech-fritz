92235adce0dffc48dfc841b662b27e5f024c8fb9
Sanawar_21 <sanawarsaeed@gmail.com>
2025-01-15 03:28:38 +0500

    await sleep

--- supervisor.py
+++ supervisor.py
@@ -34,7 +34,7 @@ async def supervisor():
     process = subprocess.Popen(["python3", "main.py"])
     logging.info("Started main.py")
 
-    asyncio.sleep(10)  # Wait for main.py to start
+    await asyncio.sleep(10)  # Wait for main.py to start
 
     while True:
         await asyncio.sleep(1)
