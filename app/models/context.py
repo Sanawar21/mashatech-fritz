14d27bd5c6852d192042c4451bc671786895f7f8
Sanawar_21 <sanawarsaeed@gmail.com>
2025-02-05 17:43:43 +0500

    implement context management

---
+++ app/models/context.py
@@ -0,0 +1,113 @@
+from io import TextIOWrapper
+from .counter import Counter
+import queue
+
+
+class Context:
+    def __init__(self,
+                 kl_prev_ads: list,
+                 pending_msgs_queue: queue.Queue,
+                 offers_sent_count: int,
+                 status_check_counter: Counter,
+                 offers_reset_counter: Counter,
+                 pending_deletion_counter: Counter,
+                 accepted_deletion_counter: Counter,
+                 catalog_refresh_counter: Counter,
+                 self_connect_counter: Counter
+                 ) -> None:
+        self.kl_prev_ads = kl_prev_ads
+        self.pending_msgs_queue = pending_msgs_queue
+        self.offers_sent_count = offers_sent_count
+        self.status_check_counter = status_check_counter
+        self.offers_reset_counter = offers_reset_counter
+        self.pending_deletion_counter = pending_deletion_counter
+        self.accepted_deletion_counter = accepted_deletion_counter
+        self.catalog_refresh_counter = catalog_refresh_counter
+        self.self_connect_counter = self_connect_counter
+
+    @classmethod
+    def new(cls):
+        status_check_counter = Counter(0, 5, 0)
+        offers_reset_counter = Counter(1, 0, 0)
+        pending_deletion_counter = Counter(48, 0, 0)
+        accepted_deletion_counter = Counter(24, 0, 0)
+        catalog_refresh_counter = Counter(0, 5, 0)
+        self_connect_counter = Counter(0, 5, 0)
+        kl_prev_ads = []
+        pending_msgs_queue = queue.Queue()
+        offers_sent_count = 0
+        obj = cls(
+            kl_prev_ads,
+            pending_msgs_queue,
+            offers_sent_count,
+            status_check_counter,
+            offers_reset_counter,
+            pending_deletion_counter,
+            accepted_deletion_counter,
+            catalog_refresh_counter,
+            self_connect_counter
+        )
+        return obj
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        kl_prev_ads = data.get("kl_prev_ads")
+
+        pending_msgs_queue = queue.Queue()
+        for item in data.get("pending_msgs_queue", []):
+            pending_msgs_queue.put(item)
+
+        offers_sent_count = data.get("offers_sent_count")
+        status_check_counter = Counter.from_dict(
+            data.get("status_check_counter"))
+        offers_reset_counter = Counter.from_dict(
+            data.get("offers_reset_counter"))
+        pending_deletion_counter = Counter.from_dict(
+            data.get("pending_deletion_counter"))
+        accepted_deletion_counter = Counter.from_dict(
+            data.get("accepted_deletion_counter"))
+        catalog_refresh_counter = Counter.from_dict(
+            data.get("catalog_refresh_counter"))
+        self_connect_counter = Counter.from_dict(
+            data.get("self_connect_counter"))
+
+        return cls(
+            kl_prev_ads,
+            pending_msgs_queue,
+            offers_sent_count,
+            status_check_counter,
+            offers_reset_counter,
+            pending_deletion_counter,
+            accepted_deletion_counter,
+            catalog_refresh_counter,
+            self_connect_counter
+        )
+
+    @classmethod
+    def from_file(cls, file: TextIOWrapper):
+        data = eval(file.read())
+        return cls.from_dict(data)
+
+    def to_dict(self):
+        return {
+            "kl_prev_ads": self.kl_prev_ads,
+            "pending_msgs_queue": list(self.pending_msgs_queue.queue),
+            "offers_sent_count": self.offers_sent_count,
+            "status_check_counter": self.status_check_counter.to_dict(),
+            "offers_reset_counter": self.offers_reset_counter.to_dict(),
+            "pending_deletion_counter": self.pending_deletion_counter.to_dict(),
+            "accepted_deletion_counter": self.accepted_deletion_counter.to_dict(),
+            "catalog_refresh_counter": self.catalog_refresh_counter.to_dict(),
+            "self_connect_counter": self.self_connect_counter.to_dict()
+        }
+
+    def save(self, file: TextIOWrapper):
+        file.write(str(self.to_dict()))
+
+    def start_counters(self):
+        self.status_check_counter.start()
+        self.offers_reset_counter.start()
+        self.pending_deletion_counter.start()
+        self.accepted_deletion_counter.start()
+        self.catalog_refresh_counter.start()
+        self.self_connect_counter.start()
--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import asyncio
-import queue
 import logging
 from datetime import datetime
+from app.models import Context
 
 
 async def main_loop():
+    ctx = None
     while True:
         try:
-            await main()
+            await main(ctx)
         except Exception as e:
             logging.error(f"Unexpected error in main loop: {e}", exc_info=True)
             # Optionally, add a short delay before restarting to prevent rapid restart loops
             await asyncio.sleep(5)
 
+            try:
+                with open("data/context.json", "r") as f:
+                    ctx = Context.from_file(f)
+            except FileNotFoundError:
+                ctx = Context.new()
 
-async def main():
+
+async def main(ctx: Context | None):
     from app.utils import get_chat_id_from_link, setup_logging
     setup_logging()  # prevents the overriden logging.basicConfig from being called
 
     from app.clients import KleinanzeigenClient, TelegramClient, AirtableClient
     from app.server import WebSocketServer
-    from app.models import Counter, Catalog
+    from app.models import Catalog
     from app.cache import MessageIDCache
     from app.messages.outgoing import SendOfferMessage, CheckOfferStatusMessage, DeleteOfferMessage, ReleasePaymentMessage
     from app.exceptions import InvalidAdException
@@ -29,48 +36,41 @@ async def main():
     tg_client = TelegramClient()
     at_client = AirtableClient()
     msg_cache = MessageIDCache()
+
     server = WebSocketServer('0.0.0.0', 8765)
     catalog = Catalog()
 
-    pending_msgs_queue = queue.Queue()  # Contains SendOfferMessage s
-    offers_sent = 0
     MAX_OFFERS_PER_HOUR = 40
 
-    # Initialize counters
-    status_check_counter = Counter(0, 5, 0)
-    offers_reset_counter = Counter(1, 0, 0)
-    pending_deletion_counter = Counter(48, 0, 0)
-    accepted_deletion_counter = Counter(24, 0, 0)
-    self_connect_counter = Counter(0, 5, 0)
-    catalog_refresh_counter = Counter(0, 5, 0)
+    if not ctx:
+        context = Context.new()
+    else:
+        context = ctx
+
+    ka_client.previous_ads = context.kl_prev_ads
 
     await server.start()
     logging.info(f"Server started at {server.public_address}")
 
-    # Start counters
-    offers_reset_counter.start()
-    status_check_counter.start()
-    pending_deletion_counter.start()
-    accepted_deletion_counter.start()
-    self_connect_counter.start()
-    catalog_refresh_counter.start()
-
     while True:
         try:
+            with open("data/context.json", "w") as f:
+                context.save(f)
+
             msg_cache.refresh()
 
-            if self_connect_counter.is_finished():
+            if context.self_connect_counter.is_finished():
                 try:
                     await server.self_connect()
                 except TimeoutError:
                     logging.error("Unable to connect to server, restarting")
                     await server.stop()
                     await server.start()
-                self_connect_counter.restart()
+                context.self_connect_counter.restart()
 
-            if catalog_refresh_counter.is_finished():
+            if context.catalog_refresh_counter.is_finished():
                 catalog.refresh()
-                catalog_refresh_counter.restart()
+                context.catalog_refresh_counter.restart()
 
             await asyncio.sleep(1)
 
@@ -90,30 +90,30 @@ async def main():
                 except InvalidAdException:
                     continue
 
-                if offers_sent <= 50:
+                if context.offers_sent_count <= 50:
                     logging.info(f"Sending offer to {ad.uid}")
                     await server.send_message(message)
                     tg_client.send_ad_alert(ad)
-                    offers_sent += 1
+                    context.offers_sent_count += 1
                 else:
                     logging.info(f"Putting {ad.uid} in queue")
-                    pending_msgs_queue.put(message)
+                    context.pending_msgs_queue.put(message)
 
             # Send pending offers
-            while not pending_msgs_queue.empty() and offers_sent <= MAX_OFFERS_PER_HOUR:
-                message = pending_msgs_queue.get()
+            while not context.pending_msgs_queue.empty() and context.offers_sent_count <= MAX_OFFERS_PER_HOUR:
+                message = context.pending_msgs_queue.get()
                 logging.info(f"Sending offer to {message.link} (from queue)")
                 tg_client.send_ad_alert(ad)
                 await server.send_message(message)
-                offers_sent += 1
+                context.offers_sent_count += 1
 
             # Reset offers sent counter
-            if offers_reset_counter.is_finished():
-                offers_sent = 0
-                offers_reset_counter.restart()
+            if context.offers_reset_counter.is_finished():
+                context.offers_sent_count = 0
+                context.offers_reset_counter.restart()
 
             # Check status of offers
-            if status_check_counter.is_finished():
+            if context.status_check_counter.is_finished():
                 cached_ids = msg_cache.read_n_day_old(2)
                 for id_ in cached_ids:
                     if id_.status != "paid":
@@ -121,10 +121,10 @@ async def main():
                             f"Sending status check message for {id_.message_id}")
                         message = CheckOfferStatusMessage(id_.message_id)
                         await server.send_message(message)
-                status_check_counter.restart()
+                context.status_check_counter.restart()
 
             # Deletion of pending offers
-            if pending_deletion_counter.is_finished():
+            if context.pending_deletion_counter.is_finished():
                 pending_offers = msg_cache.read_n_day_old(2, "pending")
                 for offer in pending_offers:
                     logging.info(
@@ -132,10 +132,10 @@ async def main():
                     message = DeleteOfferMessage(offer.message_id)
                     msg_cache.delete(offer.message_id)
                     await server.send_message(message)
-                pending_deletion_counter.restart()
+                context.pending_deletion_counter.restart()
 
             # Deletion of accepted offers that are not paid
-            if accepted_deletion_counter.is_finished():
+            if context.accepted_deletion_counter.is_finished():
                 accepted_offers = msg_cache.read_n_day_old(1, "accepted")
                 for offer in accepted_offers:
                     logging.info(
@@ -143,7 +143,7 @@ async def main():
                     message = DeleteOfferMessage(offer.message_id)
                     msg_cache.delete(offer.message_id)
                     await server.send_message(message)
-                accepted_deletion_counter.restart()
+                context.accepted_deletion_counter.restart()
 
             # Release payments
             if datetime.now().hour == 0:
